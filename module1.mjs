4b469559cef3391b2f33ca524d01ec6757fa8e52
Madhu2597 <68584787+Madhu2597@users.noreply.github.com>
2021-10-31 19:00:52 +0530

    Add files via upload

---
+++ module1.mjs
@@ -0,0 +1,5 @@
+//import { a } from "./module2.mjs";
+import dza, { a, f } from './module2.mjs'
+console.log(dza);
+console.log(a);
+console.log(f);
---
+++ module2.mjs
@@ -0,0 +1,10 @@
+const a = "twinkle";
+const b = "dimple";
+const c = "Vaibhav";
+const d = "Karan";
+const e = "rudra";
+const f = "chiku";
+
+export default c;
+export { a };
+export { f };
